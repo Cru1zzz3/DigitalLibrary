08a16067b92585f947386c01978a19fea570eae2
Cru1zzz3 <udartzev_sv@mail.ru>
2020-04-28 17:20:06 +0300

    Added registration template to do add user and hashing password to db.

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	db "github.com/Cru1zzz3/DigitalLibrary/database"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gorilla/mux"
 )
+
+type RegistrationPageData struct {
+	Username string
+	Email    string
+	Password string
+	Hash     string
+}
+
+type SuccessfullyRegistration struct {
+	Success bool
+}
+
+type IndexPageData struct {
+	PageTitle string
+}
 
 func main() {
 
@@ -26,7 +43,75 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Welcome to digital library web site!")
+		//fmt.Fprintln(w, "Welcome to digital library web site!")
+
+		tmpl, err := template.ParseFiles("../templates/index.html")
+		if err != nil {
+			log.Fatal("Error in parsing index template", err)
+		}
+
+		indexData := IndexPageData{
+			PageTitle: "Welcome to main page of DigitalLibrary!",
+		}
+
+		err = tmpl.Execute(w, indexData)
+		if err != nil {
+			log.Fatal("Error in execute index template", err)
+		}
+
+	})
+
+	r.HandleFunc("/registration", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Fprintln(w, "Registration page!")
+
+		tmpl, err := template.ParseFiles("../templates/register.html")
+		if err != nil {
+			log.Fatal("Error in parsing register template", err)
+		}
+
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return // if get it will not proceed to get values from form
+		}
+
+		// tmpl := template.Must(template.ParseFiles("../register.html"))
+
+		// data := RegistrationPageData{
+		// 	PageTitle: "This is registration page",
+		// 	Username:  "Cru1zzz3",
+		// 	Email:     "mailru",
+		// 	Password:  "admin",
+		// }
+
+		registrationData := RegistrationPageData{
+			Username: r.FormValue("username"),
+			Email:    r.FormValue("email"),
+			Password: r.FormValue("password"),
+		}
+
+		succ := SuccessfullyRegistration{
+			Success: true,
+		}
+
+		err = tmpl.Execute(w, succ)
+		if err != nil {
+			log.Fatal("Error in success template execute", err)
+		}
+
+		// TODO: add new user to DB
+		hash, _ := HashPassword(registrationData.Password)
+		registrationData.Hash = hash
+
+		fmt.Println("Password:", registrationData.Password)
+		fmt.Println("Hash:    ", registrationData.Hash)
+
+		match := CheckPasswordHash(registrationData.Password, registrationData.Hash)
+		fmt.Println("Match:   ", match)
+
+	})
+
+	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Login page!")
 
 	})
 
@@ -53,4 +138,16 @@ func main() {
 
 	http.ListenAndServe(":80", r)
 
+}
+
+//HashPassword get hash of the inputted password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+//CheckPasswordHash checks password with corresponding hash and returns true or false
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.7.4
-	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904 // indirect
+	golang.org/x/crypto v0.0.0-20200427165652-729f1e841bcc
 )
