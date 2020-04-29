ead91b34789474e6368c4a4974e514f34701cbfe
Cru1zzz3 <udartzev_sv@mail.ru>
2020-04-29 23:44:49 +0300

    Added registration + hashing pass in DB and login

--- cmd/main.go
+++ cmd/main.go
@@ -12,35 +12,37 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type RegistrationPageData struct {
-	Username string
-	Email    string
-	Password string
-	Hash     string
-}
-
-type SuccessfullyRegistration struct {
-	Success bool
-}
-
+//IndexPageData ...
 type IndexPageData struct {
 	PageTitle string
 }
 
 func main() {
 
+	r := mux.NewRouter()
+
+	r.HandleFunc("/readers", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to digital library web site!")
+		err := db.SelectReader(w)
+		if err != nil {
+			log.Fatal("Can's select Reader from DB")
+		}
+
+	})
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
 	Conn, err := db.ConnectToDB()
 	if err != nil {
 		log.Fatal("Error in connect to DB: " + err.Error())
 	}
 	defer Conn.Close()
-
-	err = db.InsertReader("cru1zzz3", "Stanislav", "Udartsev", 22)
-	if err != nil {
-		log.Fatal("Error in insert new Reader: " + err.Error())
-	}
-
-	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		//fmt.Fprintln(w, "Welcome to digital library web site!")
@@ -62,9 +64,24 @@ func main() {
 	})
 
 	r.HandleFunc("/registration", func(w http.ResponseWriter, r *http.Request) {
+
+		//RegistrationPageData ...
+		type RegistrationPageData struct {
+			Nickname string
+			Email    string
+			Password string
+			Hash     string
+		}
+
+		//SuccessfullyRegistration ...
+		type SuccessfullyRegistration struct {
+			Success  bool
+			Nickname string
+		}
+
 		//fmt.Fprintln(w, "Registration page!")
 
-		tmpl, err := template.ParseFiles("../templates/register.html")
+		tmpl, err := template.ParseFiles("../templates/registration.html")
 		if err != nil {
 			log.Fatal("Error in parsing register template", err)
 		}
@@ -78,19 +95,20 @@ func main() {
 
 		// data := RegistrationPageData{
 		// 	PageTitle: "This is registration page",
-		// 	Username:  "Cru1zzz3",
+		// 	Nickname:  "Cru1zzz3",
 		// 	Email:     "mailru",
-		// 	Password:  "admin",
+		// 	IDPassword:  "admin",
 		// }
 
 		registrationData := RegistrationPageData{
-			Username: r.FormValue("username"),
-			Email:    r.FormValue("email"),
-			Password: r.FormValue("password"),
+			Nickname: r.FormValue("Nickname"),
+			Email:    r.FormValue("Email"),
+			Password: r.FormValue("Password"),
 		}
 
 		succ := SuccessfullyRegistration{
-			Success: true,
+			Success:  true,
+			Nickname: registrationData.Nickname,
 		}
 
 		err = tmpl.Execute(w, succ)
@@ -98,7 +116,6 @@ func main() {
 			log.Fatal("Error in success template execute", err)
 		}
 
-		// TODO: add new user to DB
 		hash, _ := HashPassword(registrationData.Password)
 		registrationData.Hash = hash
 
@@ -108,46 +125,105 @@ func main() {
 		match := CheckPasswordHash(registrationData.Password, registrationData.Hash)
 		fmt.Println("Match:   ", match)
 
-	})
-
-	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Login page!")
-
-	})
-
-	r.HandleFunc("/readers", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Welcome to digital library web site!")
-		err := db.SelectReader(w)
-		if err != nil {
-			log.Fatal("Can's select Reader from DB")
+		if match {
+			err = db.RegisterUser(registrationData.Nickname, registrationData.Email, registrationData.Hash)
+			if err != nil {
+				log.Fatal("Error in register new user: " + err.Error())
+			}
 		}
 
 	})
 
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
+	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 
-		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+		type loginPage struct {
+			PageTitle string
+			Nickname  string
+			Password  string
+			Hash      string
+		}
+
+		type LoginErrors struct {
+			NotRegistered bool
+			AuthError     bool
+		}
+
+		loginPageData := loginPage{
+			PageTitle: "Login:",
+		}
+
+		tmpl, err := template.ParseFiles("../templates/login.html")
+		if err != nil {
+			log.Fatal("Error in parse login template", err)
+		}
+
+		// Will continue if submit button will be pressed!
+		if r.Method != http.MethodPost {
+			err = tmpl.Execute(w, loginPageData)
+			if err != nil {
+				log.Fatal("Error in execute login template", err)
+			}
+			return
+		}
+
+		loginPageData = loginPage{
+			Nickname: r.FormValue("Nickname"),
+			Password: r.FormValue("Password"),
+		}
+
+		checkTmpl, err := template.ParseFiles("../templates/checklogin.html")
+		if err != nil {
+			log.Fatal("Error in parse login template", err)
+		}
+
+		registered, hashFromDB, err := db.LoginUser(loginPageData.Nickname)
+		if err != nil {
+			log.Fatal("Error in check user login", err)
+		}
+		if !registered {
+			err = checkTmpl.Execute(w, LoginErrors{true, false})
+			if err != nil {
+				log.Fatal("Error in user not registered message", err)
+			}
+		} else {
+			// TODO: Compare pass and hash
+			if err != nil {
+				log.Fatal("Error in main.go checkTmpl.Execute", err)
+			}
+
+			err := bcrypt.CompareHashAndPassword([]byte(hashFromDB), []byte(loginPageData.Password))
+			if err != nil {
+				log.Println("Error in compare hashing", err)
+				err = checkTmpl.Execute(w, LoginErrors{false, true})
+				return
+			}
+
+			err = checkTmpl.Execute(w, LoginErrors{false, false})
+
+		}
+
+		// err = tmpl.Execute(w, loginPageData)
+		// if err != nil {
+		// 	log.Fatal("Error in execute login template", err)
+		// }
+
 	})
 
-	fs := http.FileServer(http.Dir("static/")) // Directory where will be FileServer
-
-	http.Handle("/static/", http.StripPrefix("/static", fs))
+	// fs := http.FileServer(http.Dir("static/")) // Directory where will be FileServer
+	// http.Handle("/static/", http.StripPrefix("/static", fs))
 
 	http.ListenAndServe(":80", r)
 
 }
 
-//HashPassword get hash of the inputted password
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+//HashPassword get hash of the inputted IDPassword
+func HashPassword(Password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(Password), 14)
 	return string(bytes), err
 }
 
-//CheckPasswordHash checks password with corresponding hash and returns true or false
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+//CheckPasswordHash checks IDPassword with corresponding hash and returns true or false
+func CheckPasswordHash(Password, Hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(Hash), []byte(Password))
 	return err == nil
 }
--- database/database.go
+++ database/database.go
@@ -1,16 +1,18 @@
 package database
 
-import(
-	"log"
-	"fmt"
+import (
 	"context"
 	"errors"
+	"fmt"
+	"log"
 
 	"net/http"
-	
+
 	"database/sql"
+
 	_ "github.com/denisenkom/go-mssqldb"
 )
+
 var server = "DESKTOP-OBVOF9A"
 var port = "1433"
 var user = "user"
@@ -20,68 +22,137 @@ var database = "DigitalLibrary"
 // Conn uses for shared connection between functions/queries
 var Conn *sql.DB
 
-
 // ConnectToDB connects to MSSQL database and returns connection to database
-func ConnectToDB() (*sql.DB,error){
-		//connString := fmt.Sprintf("server=%s,port=%s,user id=%s, password=%s,database=%s", server, port, user, password, database)
+func ConnectToDB() (*sql.DB, error) {
+	//connString := fmt.Sprintf("server=%s,port=%s,user id=%s, Iassword=%s,database=%s", server, port, user, IDPassword, database)
 	//conn, err := sql.Open("sqlserver", connString)
 
 	var err error
 	Conn, err = sql.Open("sqlserver", "sqlserver://user:user@DESKTOP-OBVOF9A?database=DigitalLibrary")
 	if err != nil {
 		log.Println("Error creating connection pool: " + err.Error())
-		return nil,err
+		return nil, err
 	}
 
 	ctx := context.Background()
-    err = Conn.PingContext(ctx)
-    if err != nil {
+	err = Conn.PingContext(ctx)
+	if err != nil {
 		log.Println(err.Error())
-		return nil,err
-    }
+		return nil, err
+	}
 	log.Printf("Connected!")
 	return Conn, nil
-	
+
 }
 
-// InsertReader uses autoincrement IDReader field
-func InsertReader(Login string,Name string,Surname string,Age int) error {
-	
-	
+//RegisterUser adds to DB new user
+func RegisterUser(Nickname string, Email string, Hash string) error {
+
 	ctx := context.Background()
 	var err error
-	
-    if Conn == nil {
-        err = errors.New("Insert reader: db is null")
-        return err
-	}
 
 	// Check if database is alive.
-    err = Conn.PingContext(ctx)
-    if err != nil {
+	if Conn == nil {
+		err = errors.New("Register user: db is null")
 		return err
 	}
-	
-	
-	// 
-	tsql := "INSERT INTO Readers (Login,Name,Surname,Age) Values (@Login,@Name,@Surname,@Age)"
+
+	err = Conn.PingContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	tsql := "INSERT INTO Users (Nickname,Email,Hash) Values (@Nickname,@Email,@Hash)"
 	prepared, err := Conn.Prepare(tsql)
-    if err != nil {
-		log.Println("Error insert row: " + err.Error(), prepared)
+	if err != nil {
+		log.Println("Error prepare new user row: "+err.Error(), prepared)
 		return err
 	}
 	defer prepared.Close()
-	
+
 	row := prepared.QueryRowContext(
-        ctx,
-        sql.Named("Login", Login),
+		ctx,
+		sql.Named("Nickname", Nickname),
+		sql.Named("Email", Email),
+		sql.Named("Hash", Hash),
+	)
+
+	var newID int64
+	err = row.Scan(&newID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("Successfully registred user with nickname", Nickname)
+		} else {
+			panic(err)
+		}
+	}
+	return nil
+}
+
+//LoginUser ...
+func LoginUser(Nickname string) (bool, string, error) {
+	ctx := context.Background()
+	var err error
+
+	// Check if database is alive.
+	if Conn == nil {
+		err = errors.New("Register user: db is null")
+		return false, "", err
+	}
+
+	err = Conn.PingContext(ctx)
+	if err != nil {
+		return false, "", err
+	}
+
+	var nicknameFromDB, hashFromDB string // FQ = from query
+
+	tsql := fmt.Sprintf("SELECT Nickname, Hash FROM Users where Nickname = '%s';", Nickname)
+	err = Conn.QueryRow(tsql, 1).Scan(&nicknameFromDB, &hashFromDB)
+	if err != nil {
+		log.Println("User is not registered!: ", err)
+		return false, "", nil
+	}
+
+	return true, hashFromDB, nil
+}
+
+// InsertReader uses autoincrement IDReader field
+func InsertReader(Login string, Name string, Surname string, Age int) error {
+
+	ctx := context.Background()
+	var err error
+
+	if Conn == nil {
+		err = errors.New("Insert reader: db is null")
+		return err
+	}
+
+	// Check if database is alive.
+	err = Conn.PingContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	//
+	tsql := "INSERT INTO Readers (Login,Name,Surname,Age) Values (@Login,@Name,@Surname,@Age)"
+	prepared, err := Conn.Prepare(tsql)
+	if err != nil {
+		log.Println("Error insert row: "+err.Error(), prepared)
+		return err
+	}
+	defer prepared.Close()
+
+	row := prepared.QueryRowContext(
+		ctx,
+		sql.Named("Login", Login),
 		sql.Named("Name", Name),
 		sql.Named("Surname", Surname),
 		sql.Named("Age", Age))
 
-    var newID int64
-    err = row.Scan(&newID)
-    if err != nil {
+	var newID int64
+	err = row.Scan(&newID)
+	if err != nil {
 		if err == sql.ErrNoRows {
 			fmt.Println("Successfully inserted new Reader!")
 		} else {
@@ -91,42 +162,74 @@ func InsertReader(Login string,Name string,Surname string,Age int) error {
 	return nil
 }
 
-// SelectReader executes select query on Readers table  
+// SelectReader executes select query on Readers table
 func SelectReader(w http.ResponseWriter) error {
 	tsql := fmt.Sprintf("SELECT * FROM Readers;")
 	rows, err := Conn.Query(tsql)
-    if err != nil {
-       log.Fatal("Error select row: " + err.Error())
+	if err != nil {
+		log.Fatal("Error select row: " + err.Error())
 	}
 	defer rows.Close()
 	var count int
 
 	for rows.Next() {
-		var IDReader,Age int
-		var Login,Name,Surname string
+		var IDReader, Age int
+		var Login, Name, Surname string
 		// Get values from row.
-        err := rows.Scan(&IDReader, &Login, &Name,&Surname,&Age)
-        if err != nil {
+		err := rows.Scan(&IDReader, &Login, &Name, &Surname, &Age)
+		if err != nil {
 			log.Fatal("Error scan row: " + err.Error())
-        }
+		}
 
-        fmt.Fprintf(w ,"ID: %d, Login: %s, Name: %s,Surname %s,Age: %d,\n", IDReader, Login, Name,Surname,Age)
-        count++
+		fmt.Fprintf(w, "ID: %d, Login: %s, Name: %s,Surname %s,Age: %d,\n", IDReader, Login, Name, Surname, Age)
+		count++
 	}
-	
+
 	return nil
 }
 
-	// db, err := db.Query("select * from dbo.Formulars")
-	// if err != nil {
-	// 	log.Fatal("Prepare failed: ", err.Error())
-	// }
-	// defer stmt.Close()
+// SelectBook TODO: Finish search via searchbar
+func SelectBook(w http.ResponseWriter) error {
+	search := "forest"
+	tsql := fmt.Sprintf(`Select NameBook,NameAuthor,NameGenre 
+	FROM Books,BookAuthor,Authors,Genres 
+	WHERE Books.IDBook = BookAuthor.IDBook
+	AND BookAuthor.IDBook = Authors.IDAuthor
+	AND Books.IDGenre = Genres.IDGenre 
+	AND (NameBook LIKE '%%%s%%' OR Authors.NameAuthor LIKE  '%%%s%%' OR NameGenre LIKE  '%%%s%%');`, search, search, search)
+	rows, err := Conn.Query(tsql)
+	if err != nil {
+		log.Fatal("Error select row: " + err.Error())
+	}
+	defer rows.Close()
 
-	// row := stmt.QueryRow()
-	// var IDBook int
-	// err = row.Scan(&IDBook)
-	// if err != nil {
-	// 	log.Fatal("Scan failed:", err.Error())
-	// }
-	// fmt.Printf("IDBook:%d\n", IDBook)
+	var count int
+
+	for rows.Next() {
+		var NameBook, NameAuthor, NameGenre string
+		// Get values from row.
+		err := rows.Scan(&NameBook, &NameAuthor, &NameGenre)
+		if err != nil {
+			log.Fatal("Error scan row: " + err.Error())
+		}
+
+		fmt.Fprintf(w, "NameBook: %s, NameAuthor: %s, NameGenre: %s,\n", NameBook, NameAuthor, NameGenre)
+		count++
+	}
+
+	return nil
+}
+
+// db, err := db.Query("select * from dbo.Formulars")
+// if err != nil {
+// 	log.Fatal("Prepare failed: ", err.Error())
+// }
+// defer stmt.Close()
+
+// row := stmt.QueryRow()
+// var IDBook int
+// err = row.Scan(&IDBook)
+// if err != nil {
+// 	log.Fatal("Scan failed:", err.Error())
+// }
+// fmt.Printf("IDBook:%d\n", IDBook)
